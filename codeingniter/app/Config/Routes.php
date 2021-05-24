979d37af1f405278320cdfe6477c2b895697a72f
goldem64zyx <83962210+goldem64zyx@users.noreply.github.com>
2021-05-24 11:34:01 -0500

    Update Routes.php

--- codeingniter/app/Config/Routes.php
+++ codeingniter/app/Config/Routes.php
@@ -34,7 +34,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/', 'LoginController::index');
-$routes->post('login', 'LoginController::login');
+$routes->post('/login', 'LoginController::login');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
